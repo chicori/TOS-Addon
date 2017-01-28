615d0f347d50b69733de16f904d87b6e13d671a7
chicori <popolo@big.jp>
2017-01-28 16:13:05 +0900

    Add files via upload

---
+++ checkblessedshard/src/checkblessedshard.lua
@@ -0,0 +1,50 @@
+﻿local addonName = "CHECKBLESSEDSHARD"
+local addonNameLower = string.lower(addonName)
+local author = "CHICORI"
+
+_G["ADDONS"] = _G["ADDONS"] or {}
+_G["ADDONS"][author] = _G["ADDONS"][author] or {}
+_G["ADDONS"][author][addonName] = _G["ADDONS"][author][addonName] or {}
+local g = _G["ADDONS"][author][addonName]
+
+local acutil = require('acutil')
+CHAT_SYSTEM(string.format("%s.lua is loaded", addonName))
+
+function CHECKBLESSEDSHARD_ON_INIT(addon, frame)
+
+	g.addon = addon
+	g.frame = frame
+
+	addon:RegisterMsg('OPEN_DLG_WAREHOUSE', 'CHECK_CHECKBLESSEDSHARD')
+end
+
+function CHECK_CHECKBLESSEDSHARD()
+
+	local findItem = 645783 --ID:Blessed Shard
+
+	local invList  = session.GetInvItemList()
+	local index    = invList:Head()
+	local count    = session.GetInvItemList():Count() -1
+
+	for i = 0, count do
+		local invItem = invList:Element(index)
+		index = invList:Next(index)
+
+		local itemObj = GetIES(invItem:GetObject())
+		if itemObj == nil then
+			break
+		end
+
+		if itemObj.ClassID == findItem then
+			local itemMsg = {
+							icon = "{img " .. itemObj.Icon .. " 30 30} "
+							,name = itemObj.Name
+							,msg  = "を所持しています。"
+							}
+		
+			ui.MsgBox(string.format("%s%s%s",itemMsg.icon
+											,itemMsg.name
+											,itemMsg.msg))
+		end
+	end
+end
